94f85a10b6a4b3bb15f01980fcc38ab1e186ea92
venkateshrajan <venkatesh.xaviers@gmail.com>
2023-11-05 14:02:54 +0530

    server and client code

--- client/Main.cpp
+++ client/Main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include "pch.h"
+
+#include <grpc/grpc.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+#include <grpcpp/support/status.h>
+#include <myproto/grpc_test.grpc.pb.h>
+#include <myproto/grpc_test.pb.h>
 
 int main (int argc, char *argv[]) {
-  std::cout << "hello from client" << std::endl;
+  google::InitGoogleLogging(argv[0]);
+
+  grpc_test::HelloRequest request;
+  grpc_test::HelloReply reply;
+  request.set_name("venky");
+
+  auto channel = grpc::CreateChannel("localhost::50051", grpc::InsecureChannelCredentials());
+  std::unique_ptr<grpc_test::Greeter::Stub> stub = grpc_test::Greeter::NewStub(channel);
+  grpc::ClientContext context;
+  grpc::Status status = stub->SayHello(&context, request, &reply);
+
+  LOG(INFO) << "Got reponse from server: " << reply.message().c_str();
+  std::cout << "Got reponse from server: " << reply.message().c_str() << std::endl;
+
   return 0;
 }
--- server/Main.cpp
+++ server/Main.cpp
@@ -1,8 +1,36 @@
 #include "pch.h"
 
+#include <grpc/grpc.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <myproto/grpc_test.grpc.pb.h>
+#include <myproto/grpc_test.pb.h>
+
+class GreeterService final : public grpc_test::Greeter::Service {
+  public:
+    virtual grpc::Status SayHello(grpc::ServerContext* context, 
+        const grpc_test::HelloRequest* request, grpc_test::HelloReply* response)  {
+      std::cout << "Server: SayHello for [" << request->name() << "]" << std::endl;
+      response->set_message("Hello, " + request->name());
+
+      return grpc::Status::OK;
+    }
+};
+
 int main (int argc, char *argv[]) {
   google::InitGoogleLogging(argv[0]);
 
-  LOG(INFO) << "Hello from server";
+  LOG(INFO) << "Server starting";
+
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
+
+  GreeterService greeter;
+  builder.RegisterService(&greeter);
+
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  server->Wait();
+  
   return 0;
 }

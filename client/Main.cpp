b2b89a0df5de75d1f59c5feb81b1ba104557d248
venkateshrajan <venkatesh.xaviers@gmail.com>
2023-11-05 22:26:23 +0530

    with uni streaming example

--- client/Main.cpp
+++ client/Main.cpp
@@ -2,11 +2,16 @@
 
 #include <absl/flags/flag.h>
 #include <absl/flags/parse.h>
+#include <absl/strings/str_format.h>
+#include <condition_variable>
+#include <glog/logging.h>
 #include <grpcpp/channel.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/create_channel.h>
 #include <grpcpp/security/credentials.h>
 #include <grpcpp/support/status.h>
+#include <grpcpp/support/sync_stream.h>
+#include <mutex>
 #include <myproto/grpc_test.grpc.pb.h>
 #include <myproto/grpc_test.pb.h>
 
@@ -25,13 +30,43 @@ class GreeterClient {
       grpc_test::HelloReply reply;
       grpc::ClientContext context;
       grpc::Status status = stub_->SayHello(&context, request, &reply);
-      if (status.ok()) 
-        return reply.message();
 
-      std::cout << status.error_code() << ":" << status.error_message()
-        << std::endl;
-      LOG(ERROR) << status.error_code() << ":" << status.error_message();
-      return "RPC failed";
+      if (!status.ok())
+      {
+        std::cout << status.error_code() << ":" << status.error_message()
+          << std::endl;
+        LOG(ERROR) << status.error_code() << ":" << status.error_message();
+        return "RPC failed";
+      }
+
+      return reply.message();
+    }
+
+    std::string SayHelloStreaming(const std::string& user) {
+      grpc_test::HelloRequest request;
+      request.set_name(user);
+
+      grpc_test::HelloReply reply;
+      grpc::ClientContext context;
+
+      std::unique_ptr<grpc::ClientReader<grpc_test::HelloReply>> reader(
+          stub_->SayHelloStreaming(&context, request));
+
+      while (reader->Read(&reply)) {
+        LOG(INFO) << "Got response from server" << reply.message();
+      }
+
+      grpc::Status status = reader->Finish();
+
+      if (!status.ok())
+      {
+        std::cout << status.error_code() << ":" << status.error_message()
+          << std::endl;
+        LOG(ERROR) << status.error_code() << ":" << status.error_message();
+        return "RPC failed";
+      }
+
+      return "RPC Succeeded";
     }
 
   private:
@@ -41,11 +76,14 @@ class GreeterClient {
 int main (int argc, char *argv[]) {
   absl::ParseCommandLine(argc, argv);
   google::InitGoogleLogging(argv[0]);
+  FLAGS_timestamp_in_logfile_name = false;
+  FLAGS_logbufsecs = 0;
+  FLAGS_logbuflevel = google::INFO;
 
   std::string target_str = absl::GetFlag(FLAGS_target);
   std::string name_str = absl::GetFlag(FLAGS_name);
   GreeterClient greeter(grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
-  LOG(INFO) << "Got reponse from server: " << greeter.SayHello(name_str);
+  LOG(INFO) << "Got reponse from server: " << greeter.SayHelloStreaming(name_str);
 
   return 0;
 }
--- server/Main.cpp
+++ server/Main.cpp
@@ -3,6 +3,7 @@
 #include <absl/flags/flag.h>
 #include <absl/flags/parse.h>
 #include <absl/strings/str_format.h>
+#include <glog/logging.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/health_check_service_interface.h>
@@ -17,9 +18,21 @@ ABSL_FLAG(uint16_t, port, 3000, "Server port for the service");
 class GreeterService final : public grpc_test::Greeter::Service {
   public:
     virtual grpc::Status SayHello(grpc::ServerContext* context, 
-        const grpc_test::HelloRequest* request, grpc_test::HelloReply* response)  {
+        const grpc_test::HelloRequest* request, grpc_test::HelloReply* response) override  {
       LOG(INFO)<< "Server: SayHello for " << request->name();
       response->set_message("Hello, " + request->name());
+
+      return grpc::Status::OK;
+    }
+
+    virtual grpc::Status SayHelloStreaming(grpc::ServerContext* context, 
+        const grpc_test::HelloRequest* request, grpc::ServerWriter<grpc_test::HelloReply>* writer) override  {
+      LOG(INFO)<< "Server: SayHelloStreaming for " << request->name();
+
+      grpc_test::HelloReply response;
+      response.set_message("Hello, " + request->name());
+      for (int i = 0; i < 10; i++)
+        writer->Write(response);
 
       return grpc::Status::OK;
     }
@@ -28,6 +41,9 @@ class GreeterService final : public grpc_test::Greeter::Service {
 int main (int argc, char *argv[]) {
   absl::ParseCommandLine(argc, argv);
   google::InitGoogleLogging(argv[0]);
+  FLAGS_timestamp_in_logfile_name = false;
+  FLAGS_logbufsecs = 0;
+  FLAGS_logbuflevel = google::INFO;
 
   grpc::EnableDefaultHealthCheckService(true);
   grpc::reflection::InitProtoReflectionServerBuilderPlugin();
